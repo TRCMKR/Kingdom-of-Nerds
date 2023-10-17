a37e5c439001f853511018d46447e70808cba358
VIPigeon <slv.bta@gmail.com>
2023-10-18 01:45:57 +0300

    Change Input without changing movement

--- Kingdom of Nerds/Assets/Scripts/PlayerMovement.cs
+++ Kingdom of Nerds/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,17 +12,45 @@ public class PlayerMovement : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     private Vector2 _direction;
 
+    private CustomInput input = null;
+    private Vector2 moveVector = Vector2.zero;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        input = new CustomInput();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += OnMovementPerformed;
+        input.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= OnMovementPerformed;
+        input.Player.Movement.canceled -= OnMovementCancelled;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+    }
+
+    private void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        // Debug.Log(moveVector);
+
+        // _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        _direction = moveVector;
 
         body.velocity = _direction * walkSpeed * Time.deltaTime;
         
