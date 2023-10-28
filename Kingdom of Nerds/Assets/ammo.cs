257a5aacfd76ead71fd717d4b0086bf479784804
Косолапов Алексей <Alkos5@yandex.ru>
2023-10-28 21:26:29 +0300

    bullet
    bullet

---
+++ Kingdom of Nerds/Assets/ammo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class ammo : MonoBehaviour
+{
+    public float speed;
+    public float destroyTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Destroyammo", destroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right*speed*Time.deltaTime);
+        
+    }
+
+    void DestroyAmmo()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Kingdom of Nerds/Assets/bullet.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float offset;
+    public Transform shotDir;
+    public GameObject ammo;
+    public float timeshot;
+    public float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
+
+        if (timeshot <= 0)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Instantiate(ammo, shotDir.position, transform.rotation);
+                timeshot = startTime;
+            }
+        }
+        else
+        {
+            timeshot -= Time.deltaTime;
+        }
+        
+
+    }
+}
