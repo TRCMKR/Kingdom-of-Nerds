a0cf65ceb575da19d75ce139003c1210bc08cd50
TRCMKR <alexplay1224@gmail.com>
2023-11-05 14:44:05 +0300

    Camera Fixes

--- Kingdom of Nerds/Assets/Scripts/Camera Logic/CameraFocus.cs
+++ Kingdom of Nerds/Assets/Scripts/Camera Logic/CameraFocus.cs
@@ -9,28 +9,29 @@ public class CameraFocus : MonoBehaviour
     private Camera cam;
     [SerializeField]
     private Transform player; 
-    [SerializeField]
-    private float minFocusDist; 
+    // [SerializeField]
+    // private float minFocusDist; 
     [SerializeField]
     private float maxFocusDist;
 
     public float smoothTime;
-    public int smoothingCoef;
 
     void FixedUpdate()
     {
-        int focusPlayer = 1;
+        Vector3 cameraPos = transform.position;
         Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 focus = Vector2.ClampMagnitude(mousePos, maxFocusDist);
 
-        if (focus.magnitude < minFocusDist)
-            focus = Vector3.zero;
-        
-        if (focus.magnitude < (minFocusDist + maxFocusDist) / 2)
-            focusPlayer = smoothingCoef;
+        // if (focus.magnitude < minFocusDist)
+        //     focus = Vector3.zero;
+
+        // if (focus.magnitude < (minFocusDist + maxFocusDist) / 2)
+        //     focusPlayer = smoothingCoef;
 
         Vector3 desiredPos = player.position + focus;
-        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothTime / focusPlayer);
+        
+        Vector3 smoothedPos = Vector3.Lerp(cameraPos, desiredPos, smoothTime); 
+        smoothedPos = Vector3.Slerp(cameraPos, smoothedPos, focus.magnitude / maxFocusDist);
 
         transform.position = smoothedPos;
     }
--- Kingdom of Nerds/Assets/Scripts/Camera Logic/CameraMovement.cs
+++ Kingdom of Nerds/Assets/Scripts/Camera Logic/CameraMovement.cs
@@ -8,28 +8,28 @@ public class CameraMovement : MonoBehaviour
     public float boundX;
     public float boundY;
     
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 delta = Vector3.zero;
 
         float deltaX = lookAt.position.x - transform.position.x;
-        if (deltaX > boundX || deltaX < -boundX)
-        {
-            if (transform.position.x < lookAt.position.x)
-                delta.x = deltaX - boundX;
-            else
-                delta.x = deltaX + boundX;
-        }
+        // if (deltaX > boundX || deltaX < -boundX)
+        // {
+        //     if (transform.position.x < lookAt.position.x)
+        //         delta.x = deltaX - boundX;
+        //     else
+        //         delta.x = deltaX + boundX;
+        // }
         
         float deltaY = lookAt.position.y - transform.position.y;
-        if (deltaY > boundY || deltaY < -boundY)
-        {
-            if (transform.position.y < lookAt.position.y)
-                delta.y = deltaY - boundY;
-            else
-                delta.y = deltaY + boundY;
-        }
+        // if (deltaY > boundY || deltaY < -boundY)
+        // {
+        //     if (transform.position.y < lookAt.position.y)
+        //         delta.y = deltaY - boundY;
+        //     else
+        //         delta.y = deltaY + boundY;
+        // }
 
-        transform.position += new Vector3(delta.x, delta.y, 0);
+        transform.position += new Vector3(deltaX, deltaY, 0);
     }
 }
