ed8bcd6ea79f673759f3114538f9ae38e1df47ab
Kostya <kostya.pavlenko05@gmail.com>
2023-11-11 20:44:35 +0300

    Add Basic Interface

--- Kingdom of Nerds/Assets/Scripts/EndingController.cs
+++ Kingdom of Nerds/Assets/Scripts/EndingController.cs
@@ -7,12 +7,17 @@ public class EndingController : MonoBehaviour
 {
     public static void ShowEnding()
     {
+        Time.timeScale = 0f;
         GameObject endPanel = GameObject.FindGameObjectWithTag("EndMenu");
         if (endPanel != null) endPanel.transform.GetChild(0).gameObject.SetActive(true);
+
+        GameObject ui = GameObject.FindGameObjectWithTag("UI");
+        if (ui != null) ui.SetActive(false);
     }
 
     public void BackToMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 }
---
+++ Kingdom of Nerds/Assets/Scripts/UIController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject player;
+    private HP playerHP;
+    private GunLogic playerGun;
+    public Slider healthBar;
+    public Slider ammoBar;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHP = player.GetComponent<HP>();
+        playerGun = player.transform.Find("Gun").GetComponent<GunLogic>();
+        healthBar.maxValue = playerHP.hp;
+        ammoBar.maxValue = playerGun.currentAmmo;
+    }
+
+    void Update()
+    {
+        healthBar.value = playerHP.hp;
+        ammoBar.value = playerGun.currentAmmo;
+    }
+
+    public void HideUI()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OpenUI()
+    {
+        gameObject.SetActive(true);
+    }
+}
