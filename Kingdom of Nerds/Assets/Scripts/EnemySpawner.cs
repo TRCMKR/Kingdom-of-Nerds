df4c2951df6946ab2b4544a235b02ef655fc7b31
Lumifois <lera.l0205@gmail.com>
2023-11-18 21:10:04 +0300

    Spawn Enemy Wave

--- Kingdom of Nerds/Assets/Scripts/EnemySpawner.cs
+++ Kingdom of Nerds/Assets/Scripts/EnemySpawner.cs
@@ -7,17 +7,22 @@ public class EnemySpawner : MonoBehaviour
     private List<GameObject> spawnEnemy;
     [SerializeField]
     private List<Transform> spawnPoint;
-    
+
     public float startTimeBtwSpawns;
     private float _timeBtwSpawns;
-    
+
     public int maxEnemies;
     public static int EnemiesNow;
     private int _enemiesSpawned;
-    
+
     private GameObject _enemy;
 
-    public bool spawn = true;
+    public bool spawn = true; // показывает, могут ли спавниться враги в данной волне
+    public bool enemiesWaves = true;  // показывает, могут ли продолжаться волны врагов
+
+    public int enemiesWavesPassed = 0;
+
+    public int maxWaves = 3;
 
     void Start()
     {
@@ -27,28 +32,60 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         int randEnemy = Random.Range(0, spawnEnemy.Count);
+
         
-        if (spawn && _enemiesSpawned < maxEnemies)
+        if (enemiesWaves && enemiesWavesPassed < maxWaves)
         {
-            if (_timeBtwSpawns < 0)
+            if (spawn && _enemiesSpawned < maxEnemies)
             {
-                _enemy = spawnEnemy[randEnemy];
-                _enemy.transform.position = spawnPoint[randEnemy].position; // пока так
-                Instantiate(_enemy);
-                _enemiesSpawned++;
-                EnemiesNow++;
-                
-                _timeBtwSpawns = startTimeBtwSpawns;
+                if (_timeBtwSpawns < 0)
+                {
+                    int randomDifficulty = Random.Range(1, 101);
+                    _enemy = spawnEnemy[randEnemy];
+                    
+                    if (randomDifficulty < 71)
+                    {
+                        Debug.Log(1);
+                    }
+                    else if (randomDifficulty > 70 && randomDifficulty <  91)
+                    {
+                        Debug.Log(2);
+                    }
+                    else
+                    {
+                        Debug.Log(3);
+                    }
+                    _enemy.transform.position = spawnPoint[randEnemy].position; // пока так
+                    Instantiate(_enemy);
+                    _enemiesSpawned++;
+                    EnemiesNow++;
+
+                    _timeBtwSpawns = startTimeBtwSpawns;
+                }
+                _timeBtwSpawns -= Time.deltaTime;
+
+                if (_enemiesSpawned == maxEnemies)
+                {
+                    spawn = false;
+                }
+
             }
 
-            _timeBtwSpawns -= Time.deltaTime;
-
-            if (_enemiesSpawned == maxEnemies)
-                spawn = false;
         }
-
+        if (enemiesWavesPassed == maxWaves)
+        {
+            enemiesWaves = false;
+        }
+        if (_enemiesSpawned == maxEnemies && EnemiesNow == 0)
+        {
+            enemiesWavesPassed++;
+            spawn = true;
+            _enemiesSpawned = 0;
+            maxEnemies++; //с каждой волной становится на одного врага больше
+        }
     }
 }
+
 
 
 
