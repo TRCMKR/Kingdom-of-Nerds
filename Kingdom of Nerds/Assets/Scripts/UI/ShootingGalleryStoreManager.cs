48d538072a02fdb3f77b306bfa272400905f70af
kostyapavr <kostya.pavlenko05@gmail.com>
2024-05-11 20:43:52 +0300

    Add more shot options, minor fixes

--- Kingdom of Nerds/Assets/Scripts/UI/ShootingGalleryStoreManager.cs
+++ Kingdom of Nerds/Assets/Scripts/UI/ShootingGalleryStoreManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ShootingGalleryStoreManager : MonoBehaviour
 {
     public GameObject storePanel;
-    public GameObject shot;
+    public GameObject shots1;
+    public GameObject shots2;
+    public GameObject shots3;
 
     public Sprite hubOn;
     private Sprite _hubOff;
 
+    public GameObject buttonHelper;
+
     public int shotPrice = 50;
+    public static int maxShots = 3;
+    public static bool gameDeclined = false;
 
     public static int PointsCount 
     {
@@ -41,38 +48,59 @@ public class ShootingGalleryStoreManager : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (gameDeclined) return;
         GetComponentInChildren<SpriteRenderer>().sprite = hubOn;
+        buttonHelper.SetActive(true);
         if (Input.GetKeyDown(KeyCode.F))
+        {
             OpenStore();
+            buttonHelper.SetActive(false);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (gameDeclined) return;
         GetComponentInChildren<SpriteRenderer>().sprite = _hubOff;
+        buttonHelper.SetActive(false);
         CloseStore();
+    }
+
+    private void CheckShotButtons()
+    {
+        shots1.GetComponent<Button>().interactable = ShotsCount < maxShots;
+        shots2.GetComponent<Button>().interactable = ShotsCount < 2;
+        shots3.GetComponent<Button>().interactable = ShotsCount < 1;
     }
 
     private void OpenStore()
     {
-        storePanel.SetActive(true);
-        shot.GetComponent<Button>().interactable = ShotsCount < 3;
-        Time.timeScale = 0f;
+        if (!gameDeclined)
+        {
+            storePanel.SetActive(true);
+            CheckShotButtons();
+            Time.timeScale = 0f;
+            PauseMenu.isPaused = true;
+        }
     }
 
     public void CloseStore()
     {
         storePanel.SetActive(false);
         Time.timeScale = 1f;
+        PauseMenu.isPaused = false;
     }
 
-    public void BuyShot()
+    public void BuyShots(int amount)
     {
-        if (PointsCount >= shotPrice)
+        int pointsNeeded = shotPrice * amount;
+
+        if (PointsCount >= pointsNeeded)
         {
-            TakePoints(shotPrice);
-            ShotsCount++;
-            
-            shot.GetComponent<Button>().interactable = ShotsCount < 3;
+            TakePoints(pointsNeeded);
+            ShotsCount += amount;
+
+            CheckShotButtons();
         }
     }
 
@@ -98,6 +126,16 @@ public class ShootingGalleryStoreManager : MonoBehaviour
 
     public void DeclineMiniGame()
     {
-        SceneManager.LoadScene($"Level 1.{Random.Range(1, 2)}");
+        CloseStore();
+        GetComponentInChildren<SpriteRenderer>().sprite = _hubOff;
+        gameDeclined = true;
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame && storePanel.activeSelf)
+        {
+            CloseStore();
+        }
     }
 }
