2350941ff3e25389149e90f8be5cf42b6c097dec
VIPigeon <slv.bta@gmail.com>
2024-05-11 21:36:13 +0300

    One bool parameter fix bug of display fo the shield public static bool isShielded = true;

--- Kingdom of Nerds/Assets/Scripts/UI/ShieldDisplay.cs
+++ Kingdom of Nerds/Assets/Scripts/UI/ShieldDisplay.cs
@@ -8,7 +8,7 @@ public class ShieldDisplay : MonoBehaviour
     public RectTransform healthBar;
     public Slider shieldSlider;
     private static int shieldHP = 0;
-    public static bool isShielded = false;
+    public static bool isShielded = true;
     public static int MaxHP = 5;
 
     public void Activate()
