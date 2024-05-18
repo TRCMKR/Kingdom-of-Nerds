94f1efa8b5dd5d7b3ade9ec12cdf9b4c78e1aaad
kostyapavr <kostya.pavlenko05@gmail.com>
2024-05-18 17:45:13 +0300

    Update store and add collected perks display

---
+++ Kingdom of Nerds/Assets/Scripts/UI/ReceivedPerksDisplay.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.SmartFormat.PersistentVariables;
+
+public class ReceivedPerksDisplay : MonoBehaviour
+{
+    public LocalizeStringEvent perksString;
+
+    private string[] perkNames = { "AutoPickUp", "BatDebuff", "RicochetBonus", "ShieldBonus" };
+
+    public void ShowPerks()
+    {
+        gameObject.SetActive(true);
+        PauseMenu.isPaused = true;
+        Time.timeScale = 0;
+
+        foreach (var perk in perkNames)
+        {
+            BoolVariable hasPerk = new BoolVariable();
+            hasPerk.Value = PlayerPrefs.GetInt(perk, 0) == 1;
+            perksString.StringReference[perk] = hasPerk;
+        }
+        perksString.RefreshString();
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame) Close();
+    }
+
+    public void Close()
+    {
+        PauseMenu.isPaused = false;
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
