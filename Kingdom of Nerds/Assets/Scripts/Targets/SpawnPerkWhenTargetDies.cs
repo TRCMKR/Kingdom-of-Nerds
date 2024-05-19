819fc41b357cd52eb879b0407678b6af68df7220
TRCMKR <alexplay1224@gmail.com>
2024-05-19 22:17:52 +0300

    ALL FIXED! RELEASE SOON!

--- Kingdom of Nerds/Assets/Scripts/Targets/SpawnPerkWhenTargetDies.cs
+++ Kingdom of Nerds/Assets/Scripts/Targets/SpawnPerkWhenTargetDies.cs
@@ -19,15 +19,11 @@ public class SpawnPerkWhenTargetDies : MonoBehaviour
 
     private void SpawnPerk(GameObject corpse)
     {
-        GameObject perk = Perks[0];
-        int ball = UnityEngine.Random.Range(1, 101);
+        GameObject perk;
+        int ball = UnityEngine.Random.Range(0, (Perks.Count) * 100 - 95);
         // There are no plans to balance perk drops, so I implemented the logic using magic numbers
-        if (ball <= 32)
-            perk = Perks[1];
-        else if (ball <= 64)
-            perk = Perks[2];
-        else if (ball <= 96)
-            perk = Perks[3];
+        perk = Perks[ball / 100];
+        Perks.RemoveAt(ball / 100);
 
         perk.transform.position = gameObject.transform.position;
         Instantiate(perk);
